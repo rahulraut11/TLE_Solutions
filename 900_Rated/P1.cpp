56a034fab523c7af400efc62beca9ca3dc070628
rahulraut11 <rahulraut110506@gmail.com>
2025-06-27 00:46:28 +0530

    added soln

--- 900_Rated/P1.cpp
+++ 900_Rated/P1.cpp
@@ -1,3 +1,20 @@
+/* Basically we have to find common position of knight from king and queen.
+
+So for king/queen there are 8 positions from which a knight can attack if a!=b
+
+(if a=b then 4 pos)
+
+we have to take these 8 positions for king and queen , and count how many are common.
+
+Use 2 sets of pair data structure to store the 8 positions.
+
+For 4 quadrants declare dx[4] and dy[4] globally and give them values of +-1 accordingly.
+
+then insert the values in set accordingly for x and y by once giving x->a ,y->b then vice versa , do this for all 4 quadrants.
+
+after that run a loop to check for all pos of kings if they exist in queen pos
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 int dx[4] = {1, -1, 1, -1};
