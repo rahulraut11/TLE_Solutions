3b35fefb2a74f9c40c2936ded017b44947a72fe4
rahulraut11 <rahulraut110506@gmail.com>
2025-06-29 00:01:03 +0530

    added soln

--- 900_Rated/P31.cpp
+++ 900_Rated/P31.cpp
@@ -1,12 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int solve()
+{
+    int n;
+    cin >> n;
+    int x = n;
+    int power2 = 0;
+    int power3 = 0;
+    while (x % 2 == 0)
+    {
+        x /= 2;
+        power2++;
+    }
+    while (x % 3 == 0)
+    {
+        x /= 3;
+        power3++;
+    }
+    if (x != 1)
+        return -1;
+    if (power3 < power2)
+        return -1;
+    else if (power3 == power2)
+        return power3;
+    else
+        return (2 * power3 - power2);
+}
+
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        // code
+        cout << solve() << endl;
     }
     return 0;
 }
